54ed6e39f1e28278b954e6f4e5f168d3668d5581
Salvian Reynaldi <salvian@traveloka.com>
2018-02-20 12:52:47 +0700

    first working image

--- get-random-subnet.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-IFS=',' read -ra AWS_SUBNET_IDS <<< "${AWS_SUBNET_IDS_STRING}"
-APP_TEMPLATE_INSTANCE_SUBNET_ID=${AWS_SUBNET_IDS[$(($(date +%s) % ${#AWS_SUBNET_IDS[@]}))]}
-echo ${APP_TEMPLATE_INSTANCE_SUBNET_ID}
